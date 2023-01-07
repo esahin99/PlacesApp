7274cee231b326c3b3b4316132653c870d7d3359
Emre Sahin <mehmetemre9906@gmail.com>
2023-01-07 21:56:15 +0300

    Updated

--- PlacesApp/ViewController.swift
+++ PlacesApp/ViewController.swift
@@ -15,10 +15,30 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.requestWhenInUseAuthorization()
         locationManager.startUpdatingLocation()
+        
+        let gestureRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(selectLocation(gestureRecognizer:)))
+        gestureRecognizer.minimumPressDuration = 2
+        mapView.addGestureRecognizer(gestureRecognizer)
     }
     
+    //mark a point on the map with touch
+    @objc func selectLocation(gestureRecognizer: UILongPressGestureRecognizer){
+        if gestureRecognizer.state == .began{
+            let touchPoint = gestureRecognizer.location(in: mapView)
+            let touchCoordinate = mapView.convert(touchPoint, toCoordinateFrom: mapView)
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = touchCoordinate
+            annotation.title = "User Selection"
+            mapView.addAnnotation(annotation)
+        }
+    }
+    
+    //shows the current location
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        print(locations[0].coordinate.latitude)
+        let location = CLLocationCoordinate2D(latitude: locations[0].coordinate.latitude, longitude: locations[0].coordinate.longitude)
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        let region = MKCoordinateRegion(center: location, span: span)
+        mapView.setRegion(region, animated: true)
     }
 
 
